bd877ebc7851fe516e959bccbaab8cd710388ab2
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-18 19:44:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@
 
 def fizzbuzz(number)
   if (number % 3 == 0)
-    return "Fizz"
+    "Fizz"
   end
   elsif (number % 5 == 0)
-    return "Buzz"
+    "Buzz"
   end
   elsif ((number % 3 == 0) && (number % 5 == 0))
-    return "FizzBuzz"
+    "FizzBuzz"
   end
   else
-    return nil
+    nil
   end
 end
